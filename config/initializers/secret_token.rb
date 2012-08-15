af8b8d5c0f4f9a47ed2e7e6d4a1b8160b134783b
Laser Nite <lasernite@gmail.com>
2012-08-15 16:00:20 -0500

    All

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Binghimons::Application.config.secret_token = '782999789f8d30e6b249effcbf08efd9dd6cf7a7252fb55a4cb969f0d9f7a7a27bfe575835dbf0745750cb5f0d5767cad3a7f4f2925e315b2a4ff4664a2ab287'
